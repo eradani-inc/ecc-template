166072faf0f5c7ddba2c3a1731c1067e933b4f8c
David Romo <dave@eradani.com>
2020-10-08 14:34:02 -0700

    Fix DSPTRFC JavaScript error handling
    Handle errors reported by the web service.

--- src/services/traffic.js
+++ src/services/traffic.js
@@ -1,42 +1,43 @@
 const axios = require("axios");
 const { traffic, ecclient } = require("../config");
 const { ECClient } = require("@eradani-inc/ec-client");
-const interface = require('./vinapi');
+const interface = require("./trfcapi");
 const response = new ECClient(ecclient);
 
 const axiosInstance = axios.create(traffic);
 
 exports.getTrafficData = async (reqkey, data) => {
-  console.log('TrafficAPI:', 'Got data', data);
+  console.log("TrafficAPI:", "Got data", data);
   // get parameters from incomming data buffer
   const compareData = interface.convertCompareToObject(data);
-  console.log('TrafficAPI:', 'Parsed data', compareData);
+  console.log("TrafficAPI:", "Parsed data", compareData);
 
   // call web service
   let result;
   let nextReqKey = reqkey;
   try {
-    console.log('TrafficAPI:', 'Sending Request', "/traffic/6.1/flow.json", {
+    console.log("TrafficAPI:", "Sending Request", "/traffic/6.1/flow.json", {
       params: {
-        bbox: '37.8929,-122.3016;37.8851,-122.2744',
-        apiKey: traffic.apiKey
-      }
+        bbox: "37.8929,-122.3016;37.8851,-122.2744",
+        apiKey: traffic.apiKey,
+      },
     });
     result = await axiosInstance.get("/traffic/6.1/flow.json", {
       params: {
-        bbox: '37.8929,-122.3016;37.8851,-122.2744',
-        apiKey: traffic.apiKey
-      }
+        bbox: "37.8929,-122.3016;37.8851,-122.2744",
+        apiKey: traffic.apiKey,
+      },
     });
   } catch (err) {
-    console.log('TrafficAPI:', 'Got ERROR!', err);
+    console.log("TrafficAPI:", "Got ERROR!", err);
     if (err.response) {
       // If the request was made and the server responded with a status code
       // that falls out of the range of 2xx
       // Note: These error formats are dependent on the web service
-      return response.sendObjectToCaller({
+      return response.sendObjectToCaller(
+        {
           httpStatus: err.response.status,
-          message: err.response.data.message
+          message: err.response.data.error_description
         },
         interface.convertObjectToResponse,
         nextReqKey
@@ -46,21 +47,22 @@ exports.getTrafficData = async (reqkey, data) => {
     // Else the request was made but no response was received
     // Note: This error format has nothing to do with the web service. This is
     // mainly TCP/IP errors.
-    return response.sendObjectToCaller({
+    return response.sendObjectToCaller(
+      {
         httpStatus: 999,
-        message: err.message
+        message: err.message,
       },
       interface.convertObjectToResponse,
       nextReqKey
     );
   }
 
-  console.log('TrafficAPI:', 'Got Result from API call', result);
+  console.log("TrafficAPI:", "Got Result from API call", result);
 
   try {
     let intersection;
     for (let road of result.data.RWS[0].RW) {
-      if (road.LI === '105-00419') {
+      if (road.LI === "105-00419") {
         intersection = road;
         break;
       }
@@ -74,10 +76,10 @@ exports.getTrafficData = async (reqkey, data) => {
         averageSpeed: road.CF[0].SP,
         length: road.TMC.LE,
         jamFactor: road.CF[0].JF * 10,
-        confidence: road.CF[0].CN * 100
+        confidence: road.CF[0].CN * 100,
       });
     }
-    
+
     roads.sort((a, b) => {
       if (a.averageSpeed > b.averageSpeed) {
         return -1;
@@ -92,27 +94,29 @@ exports.getTrafficData = async (reqkey, data) => {
       roads[i].rank = i + 1;
     }
 
-    nextReqKey = await response.sendObjectToCaller({
+    nextReqKey = await response.sendObjectToCaller(
+      {
         httpStatus: 200,
-        message: 'API Call Succeeded'
+        message: "API Call Succeeded",
       },
       interface.convertObjectToResponse,
       nextReqKey
     );
 
     // Send success result to client
-    
-    console.log('TrafficAPI:', 'Sending success response', roads);
+
+    console.log("TrafficAPI:", "Sending success response", roads);
     return response.sendObjectsToCaller(
       roads,
       interface.convertObjectToTraffic,
       nextReqKey
     );
-  } catch(e) {
-    console.log('TrafficAPI:', 'Failed parsing results', e);
-    return response.sendObjectToCaller({
+  } catch (e) {
+    console.log("TrafficAPI:", "Failed parsing results", e);
+    return response.sendObjectToCaller(
+      {
         httpStatus: 404,
-        message: 'No Route Found'
+        message: "No Route Found",
       },
       interface.convertObjectToResponse,
       nextReqKey
