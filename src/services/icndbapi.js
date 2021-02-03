1b21db0c0513e400b667d4cafde9851845b20deb
David Romo <dave@eradani.com>
2021-02-03 12:02:11 -0800

    Update icndbapi yaml

--- src/services/icndbapi.js
+++ src/services/icndbapi.js
@@ -1,34 +1,46 @@
-// Module icndbapi
+// Module: icndbapi
+// Generated source -- do not modify
 
-function toIndicator(b) {
-  return b ? "1" : "0";
-}
+const { ibmiConversions } = require("@eradani-inc/ec-client");
+const {
+  fromIbmiDate,
+  fromIbmiTime,
+  fromIbmiTimestamp,
+  toIbmiDate,
+  toIbmiTime,
+  toIbmiTimestamp
+} = ibmiConversions;
 
-exports.convertDataToObject = dataIn => {
+  // Convert Data record to JavaScript object
+exports.convertDataToObject = function convertDataToObject(dataIn) {
   // Initialize the request object
-  const dataOut = {};
+  const dataOut =   {
+  
+    };
 
   // Convert fields in record as string to fields in object
   dataOut.limitTo = dataIn.substring(0, 15).trimEnd();
 
   // Return the request as an object
   return dataOut;
-};
+}
 
-exports.convertObjectToRetData = dataIn => {
+  // Convert JavaScript object to RetData record
+exports.convertObjectToRetData = function convertObjectToRetData(dataIn) {
   // Initialize the response record as string
   let dataOut = "";
 
   // Convert fields in object to fields in record as string
-  dataOut += dataIn.httpstatus.toString().substring(0, 3).padEnd(3);
+  dataOut += dataIn.httpstatus.toFixed(0).substring(0, 5).padEnd(5);
   dataOut += dataIn.type.substring(0, 16).padEnd(16);
   dataOut += dataIn.value;
 
   // Return the response record as a string
   return dataOut;
-};
+}
 
-exports.convertObjectToRetData2 = dataIn => {
+  // Convert JavaScript object to RetData2 record
+exports.convertObjectToRetData2 = function convertObjectToRetData2(dataIn) {
   // Initialize the response record as string
   let dataOut = "";
 
@@ -37,16 +49,17 @@ exports.convertObjectToRetData2 = dataIn => {
 
   // Return the response record as a string
   return dataOut;
-};
+}
 
-exports.convertObjectToRetData3 = dataIn => {
+  // Convert JavaScript object to RetData3 record
+exports.convertObjectToRetData3 = function convertObjectToRetData3(dataIn) {
   // Initialize the response record as string
   let dataOut = "";
 
   // Convert fields in object to fields in record as string
-  dataOut += dataIn.httpstatus.toString().substring(0, 3).padEnd(3);
+  dataOut += dataIn.httpstatus.toFixed(0).substring(0, 5).padEnd(5);
   dataOut += dataIn.error;
 
   // Return the response record as a string
   return dataOut;
-};
+}
