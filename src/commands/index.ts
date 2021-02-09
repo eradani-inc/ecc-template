09466b42bd8c048cfcca9d4d34235a410f146f73
David Romo <dave@eradani.com>
2021-02-08 18:44:00 -0800

    Add TypeScript version of display traffic

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,10 +1,15 @@
 import ECCRouter from '@eradani-inc/ecc-router';
 import registerJokes from './jokes';
+import registerTraffic from './traffic';
 
 export default async function registerCommands(router: ECCRouter) {
     const jokes = new ECCRouter.Router();
     registerJokes(jokes);
     router.use('jokes', jokes);
+
+    const traffic = new ECCRouter.Router();
+    registerTraffic(traffic);
+    router.use('traffic', traffic);
 
     return router;
 }
